9bcd7a5f3ead1e216b98916b3546701047a78cc5
chessparov <154456871+chessparov@users.noreply.github.com>
2024-02-14 17:13:55 +0100

    Initial commit

---
+++ Orders.py
@@ -0,0 +1,31 @@
+import Facility
+import datetime
+import uuid
+
+
+class TOrder:
+    '''
+
+    Defines an order
+
+    '''
+
+    def __init__(self, *args):
+        self.__uuid = str(uuid.uuid4())
+        self.__date = datetime.datetime.now()
+        self.__art = list()
+        for item in args:
+            if isinstance(item, Facility.TItem):
+                self.__art.append(item)
+            else:
+                print('Invalid input! ')
+
+
+    def getDate(self):
+        return self.__date
+
+    def getArtlist(self):
+        return self.__art
+
+    def getUuid(self):
+        return self.__uuid
